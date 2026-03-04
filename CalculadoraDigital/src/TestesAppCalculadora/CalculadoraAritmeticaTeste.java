846a811f03c686175df5b2dc0ec66b0321bbf1c3
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-03 21:41:41 -0300

    feat: calculadora digital teste com junit

---
+++ CalculadoraDigital/src/TestesAppCalculadora/CalculadoraAritmeticaTeste.java
@@ -0,0 +1,36 @@
+package TestesAppCalculadora;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import FontesAppCalculadora.CalculadoraAritmetica;
+
+public class CalculadoraAritmeticaTeste {
+
+	//Atributos de comparação de resultado
+	private int resultadoReal;
+	private int resultadoEsperado;	
+	
+	@Test
+	public void testeSomar2com3() {
+		resultadoEsperado = 5;
+		CalculadoraAritmetica calc = new CalculadoraAritmetica();
+		resultadoReal = calc.somar(2, 3);assertEquals(resultadoEsperado, resultadoReal);
+	}
+	
+	@Test
+	public void testeSomar3com3() {
+		resultadoEsperado = 6;
+		CalculadoraAritmetica calc = new CalculadoraAritmetica();
+		resultadoReal = calc.somar(3, 3);assertEquals(resultadoEsperado, resultadoReal);
+	}
+	
+	@Test
+	public void testeSomar5com3() {
+		resultadoEsperado = 8;
+		CalculadoraAritmetica calc = new CalculadoraAritmetica();
+		resultadoReal = calc.somar(5, 3);assertEquals(resultadoEsperado, resultadoReal);
+	}
+
+}
